b7f3eab2ab0027261b3e21312c9718a16a492031
Kenedy-Henrique <kenedyhenrique.buenosilva@gmail.com>
2020-10-06 18:35:29 -0300

    Module to create task divs inside tasks container

---
+++ src/createTaskDivs.js
@@ -0,0 +1,41 @@
+function createTaskDivs(tabsObj, currentTab, tasksContainer){
+    for(let key in tabsObj[currentTab]){
+        let taskDiv = document.createElement('div');
+        taskDiv.classList.add('taskDiv');
+
+        let title = document.createElement('p');
+        title.classList.add('taskDivTitle');
+        title.innerText = tabsObj[currentTab][key].title;
+        taskDiv.appendChild(title);
+
+        let dueDate = document.createElement('p');
+        dueDate.classList.add('taskDivDueDate');
+        dueDate.innerHTML = tabsObj[currentTab][key].dueDate;
+        taskDiv.appendChild(dueDate);
+
+        let description = document.createElement('button');
+        description.classList.add('descriptionButton');
+        description.innerHTML = 'Show details';
+        description.addEventListener('click', () => {
+            alert(tabsObj[currentTab][key].description);
+        })
+        taskDiv.appendChild(description);
+
+        let priority = document.createElement('div');
+        priority.classList.add('divPriority');
+        priority.style.backgroundColor = tabsObj[currentTab][key].priority;
+        taskDiv.appendChild(priority);
+
+        let deleteBtn = document.createElement('button');
+        deleteBtn.classList.add('deleteBtn');
+        taskDiv.appendChild(deleteBtn);
+        deleteBtn.addEventListener('click', () => {
+            tasksContainer.removeChild(taskDiv);
+            delete tabsObj[currentTab][key];
+        })
+
+        tasksContainer.appendChild(taskDiv);
+    }
+}
+
+export default createTaskDivs;
