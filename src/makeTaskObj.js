22a7bd402429d1b2dfb52241fe58676a69b40a4c
Kenedy-Henrique <kenedyhenrique.buenosilva@gmail.com>
2020-10-05 23:26:37 -0300

    update

--- src/makeTaskObj.js
+++ src/makeTaskObj.js
@@ -1,10 +1,9 @@
-function makeTaskObj(title, description, dueDate, priority, isDone){
+function makeTaskObj(title, description, dueDate, priority){
     let obj = {};
     obj.title = title;
     obj.description = description;
     obj.dueDate = dueDate;
     obj.priority = priority;
-    obj.isDone = isDone;
 
     return obj;
 }
