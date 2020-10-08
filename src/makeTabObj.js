f2fb8bf5a9486cae7bdab58f3b857651a2543d41
Kenedy-Henrique <kenedyhenrique.buenosilva@gmail.com>
2020-10-07 21:22:42 -0300

    local storage tabsObj set and update

--- src/makeTabObj.js
+++ src/makeTabObj.js
@@ -1,5 +1,6 @@
 function makeTabObj(tabsObj, newTabName){
     tabsObj[newTabName] = {};
+    localStorage.setItem('tabsObj', JSON.stringify(tabsObj));
 }
 
 export default makeTabObj;
--- src/putTaskObjInsideTab.js
+++ src/putTaskObjInsideTab.js
@@ -1,5 +1,6 @@
 function putTaskObjInsideTab(tabsObj, currentTab, taskName, taskObj){
     tabsObj[currentTab][taskName] = taskObj;
+    localStorage.setItem('tabsObj', JSON.stringify(tabsObj));
 }
 
 export default putTaskObjInsideTab;
